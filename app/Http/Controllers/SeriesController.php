eeb238f73190263d3fbe46803fcdf6ed807b4f4d
jonasluis <jonasluis66@gmail.com>
2025-06-22 12:57:22 -0300

    utilizar  DB Facade para criar e buscar series

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -4,21 +4,26 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
     public function index(){
-        $series = [
-            'Punisher',
-            'Lost',
-            'Greys Anatomy'
-        ];
+        $series = DB::select('SELECT nome from series;');
         return view('series.index')->with('series', $series);
     }
     
     public function create(){
         return view('series.create');
-
     }
 
+    public function store(Request $request){
+        $nomeSerie = $request->input('nome');
+
+        if(DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie])){
+            return "ok";
+        } else {
+            return "Deu Erro";
+        }
+    }
 }
