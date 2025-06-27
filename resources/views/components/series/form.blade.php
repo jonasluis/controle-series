ba0264b15758ef3f1988df49dd8df28bc72f450b
jonasluis <jonasluis66@gmail.com>
2025-06-26 21:29:43 -0300

    Exibindo erros

--- resources/views/components/series/form.blade.php
+++ resources/views/components/series/form.blade.php
@@ -1,15 +1,15 @@
 <form action="{{$action}}" method="post">
         @csrf
-        @isset($nome)
+        @if($update)
         @method('PUT')
-        @endisset
+        @endif
 
         <div class="mb-3">
             <label for="nome" class="form-label">Nome:</label>
-            <input 
-                type="text" 
-                id="nome" 
-                name="nome" 
+            <input
+                type="text"
+                id="nome"
+                name="nome"
                 class="form-control"
                 @isset($nome)value="{{$nome}}"@endisset>
         </div>
