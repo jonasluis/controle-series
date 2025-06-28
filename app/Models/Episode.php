e1ed550e25d3f602f0558fb9a1adba3766a1d6f6
jonasluis <jonasluis66@gmail.com>
2025-06-28 06:16:35 -0300

    criando Temporadas e episódios e fazendo os relacionamentos entre as classes

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -4,16 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+
 use App\Models\Season;
 
-class Serie extends Model
+class Episode extends Model
 {
     use HasFactory;
-    protected $fillable = ['nome'];
 
 
-    public function temporadas(){
-        return $this->hasMany(Season::class);
+    public function season(){
+        return $this->belongsTo(Season::class);
     }
-    
 }
